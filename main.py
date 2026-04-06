b14bef8ddfde277470146f077a231f1b72f900d4
drintz36 <sorintdyz36@gmail.com>
2026-04-06 15:07:05 +0700

    hotfix/4: i have fixed on substract function to activate this function in main.py file

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
         if choice == '0': print("Goodbye!"); break
         try:
             if   choice == '1':  a,b = _ab(); print(calculator.add(a,b))
-            # elif choice == '2':  a,b = _ab(); print(calculator.subtract(a,b)) # hotfix 1
+            elif choice == '2':  a,b = _ab(); print(calculator.subtract(a,b)) # hotfix 1
             # elif choice == '3':  a,b = _ab(); print(calculator.multiply(a,b)) # hotfix 2
             elif choice == '4':  a,b = _ab(); print(calculator.divide(a,b))
             # elif choice == '5':  a,b = _ab(); print(calculator.power(a,b)) # feature 3
